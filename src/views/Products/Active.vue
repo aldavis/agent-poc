536e12b371fbb1a4cd8a6a5191f155918a61eb25
alex davis <alex.davis@fbitn.com>
2019-01-24 13:40:34 -0600

    flushing out products page a little

--- src/views/Products/Active.vue
+++ src/views/Products/Active.vue
@@ -9,9 +9,13 @@
         v-bind:title="category.name"
       >
         <v-flex hover v-for="product in category.products" :key="product.name">
-          <v-btn flat color="primary">
+          <v-btn flat color="primary" @click="showProduct(category,product)">
             <h4>{{product.name}}</h4>
           </v-btn>
+          <div v-if="product.visible">
+            {{product.description}}
+            <v-btn color="green">Start Application</v-btn>
+          </div>
         </v-flex>
       </card>
       <!-- <v-container fluid grid-list-md>
@@ -43,10 +47,16 @@ export default {
           name: "Universal Life",
           products: [
             {
-              name: "FlexLife UL"
+              name: "FlexLife UL",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             },
             {
-              name: "Platinum UL"
+              name: "Platinum UL",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             }
           ]
         },
@@ -54,16 +64,28 @@ export default {
           name: "Term",
           products: [
             {
-              name: "Secure Term ART"
+              name: "Secure Term ART",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             },
             {
-              name: "Secure Term 10"
+              name: "Secure Term 10",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             },
             {
-              name: "Secure Term 20"
+              name: "Secure Term 20",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             },
             {
-              name: "Secure Term 30"
+              name: "Secure Term 30",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             }
           ]
         },
@@ -71,12 +93,26 @@ export default {
           name: "Whole Life",
           products: [
             {
-              name: "Summit Whole Life"
+              name: "Summit Whole Life",
+              description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit urna et tincidunt porta. Ut non massa quis enim cursus cursus. Aenean vitae orci ac mi semper scelerisque quis quis arcu. Donec tempor auctor tortor nec efficitur. Praesent rutrum tortor diam, a pharetra diam pretium at. Nulla tempus venenatis purus, at egestas felis elementum et. Etiam varius fermentum turpis, vel bibendum dui aliquam at. Nunc efficitur mi in nibh iaculis, sed rhoncus libero mollis. Fusce commodo dictum nunc, at feugiat sapien porta non. In bibendum eu ante sit amet varius. Sed lorem risus, commodo vitae faucibus id, condimentum commodo felis. Nam augue felis, rhoncus at turpis sed, fringilla blandit elit. Vivamus a lorem luctus elit gravida vulputate sed a nisl. Aenean ligula nulla, semper ac suscipit pellentesque, vulputate sit amet enim",
+              visible: false
             }
           ]
         }
       ]
     };
+  },
+  methods: {
+    showProduct(category, product) {
+      console.log(category.name);
+      console.log(product.name);
+      if (this.categories[0].products[0].visible === true) {
+        this.categories[0].products[0].visible = false;
+      } else {
+        this.categories[0].products[0].visible = true;
+      }
+    }
   }
 };
 </script>
